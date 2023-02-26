2229cd5b4151a1dbd9c731cca535308acabdb7a9
Jeffrey Burt <jeffrey.m.burt@gmail.com>
2023-02-26 11:49:47 -0800

    update react

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,12 @@
 import React, { Suspense } from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { App } from './App';
 import './index.css';
-ReactDOM.render(
+
+const container = document.getElementById('root');
+const root = createRoot(container!); // createRoot(container!) if you use TypeScript
+root.render(
   <Suspense fallback={'Loading'}>
     <App />
-  </Suspense>,
-  document.getElementById('root')
+  </Suspense>
 );
