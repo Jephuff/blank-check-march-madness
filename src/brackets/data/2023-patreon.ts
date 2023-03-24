57345b2a90a468eb2a0b6384294c0c652f1eef3b
Jeffrey Burt <jeffrey.m.burt@gmail.com>
2023-03-24 10:25:55 -0700

    update

--- src/brackets/data/2023-patreon.ts
+++ src/brackets/data/2023-patreon.ts
@@ -133,7 +133,7 @@ const data: Data<0> = {
           poll: undefined,
           options: [
             {
-              winner: undefined,
+              winner: 'Mortal Kombat',
               poll: 'https://www.patreon.com/posts/march-madness-23-80436732',
               options: [
                 {
@@ -150,7 +150,7 @@ const data: Data<0> = {
             },
             {
               winner: undefined,
-              poll: undefined,
+              poll: 'https://www.patreon.com/posts/march-madness-24-80485078',
               options: [
                 {
                   winner: 'The Beatles',
--- src/brackets/data/2023.ts
+++ src/brackets/data/2023.ts
@@ -109,7 +109,7 @@ const data: Data<0> = {
           winner: undefined,
           options: [
             {
-              winner: undefined,
+              winner: 'Bong Joon-Ho',
               options: [
                 {
                   winner: 'Bong Joon-Ho',
