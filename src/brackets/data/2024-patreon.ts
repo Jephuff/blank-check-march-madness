056bd1dcc5ef96deea92e88ca84a223ddeb36e73
Jeffrey Burt <jeffrey.m.burt@gmail.com>
2024-03-10 23:38:50 -0400

    update

---
+++ src/brackets/data/2024-patreon.ts
@@ -0,0 +1,174 @@
+import { Data } from 'brackets/types';
+
+const data: Data<0> = {
+  winner: undefined,
+  poll: undefined,
+  options: [
+    {
+      winner: undefined,
+      poll: undefined,
+      options: [
+        {
+          winner: undefined,
+          poll: undefined,
+          options: [
+            {
+              winner: undefined,
+              poll: undefined,
+              options: [
+                {
+                  winner: 'The Addams Family',
+                  poll: 'https://www.patreon.com/posts/march-madness-1-99529412',
+                  options: ['My Big Fat Greek Wedding', 'The Addams Family'],
+                },
+                {
+                  winner: 'Tabletop Games',
+                  poll: undefined,
+                  options: ['Jumanji', 'Tabletop Games'],
+                },
+              ],
+            },
+            {
+              winner: undefined,
+              poll: undefined,
+              options: [
+                {
+                  winner: 'Jay Ward',
+                  poll: undefined,
+                  options: ['Alvin & The Chipmunks', 'Jay Ward'],
+                },
+                {
+                  winner: undefined,
+                  poll: undefined,
+                  options: ['Richard Curtis', 'Bridget Jones'],
+                },
+              ],
+            },
+          ],
+        },
+        {
+          winner: undefined,
+          poll: undefined,
+          options: [
+            {
+              winner: undefined,
+              poll: undefined,
+              options: [
+                {
+                  winner: 'The Equalizer',
+                  poll: 'https://www.patreon.com/posts/march-madness-3-99657102',
+                  options: ['The Equalizer', 'Rambo'],
+                },
+                {
+                  winner: 'Ernest',
+                  poll: undefined,
+                  options: ['Ernest', 'Pauly Shore'],
+                },
+              ],
+            },
+            {
+              winner: undefined,
+              poll: undefined,
+              options: [
+                {
+                  winner: undefined,
+                  poll: undefined,
+                  options: ['Jack Ryan', 'Robert Langdon'],
+                },
+                {
+                  winner: undefined,
+                  poll: undefined,
+                  options: ['Conan', 'Highlander'],
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+    {
+      winner: undefined,
+      poll: undefined,
+      options: [
+        {
+          winner: undefined,
+          poll: undefined,
+          options: [
+            {
+              winner: undefined,
+              poll: undefined,
+              options: [
+                {
+                  winner: 'Star Trek: Next Gen',
+                  poll: 'https://www.patreon.com/posts/march-madness-2-99595491',
+                  options: ['Star Trek: Next Gen', 'Body Snatchers'],
+                },
+                {
+                  winner: 'Robin Hoods',
+                  poll: undefined,
+                  options: ['King Arthurs', 'Robin Hoods'],
+                },
+              ],
+            },
+            {
+              winner: undefined,
+              poll: undefined,
+              options: [
+                {
+                  winner: undefined,
+                  poll: 'https://www.patreon.com/posts/march-madness-10-100097801',
+                  options: ['Clive Barker', 'Cube'],
+                },
+                {
+                  winner: undefined,
+                  poll: undefined,
+                  options: ['Spy Kids', 'Mexico Trilogy'],
+                },
+              ],
+            },
+          ],
+        },
+        {
+          winner: undefined,
+          poll: undefined,
+          options: [
+            {
+              winner: undefined,
+              poll: undefined,
+              options: [
+                {
+                  winner: 'Happy Madison',
+                  poll: undefined,
+                  options: ['Happy Madison', 'Stand-up Animation'],
+                },
+                {
+                  winner: 'American Pie',
+                  poll: undefined,
+                  options: ['American Pie', 'Major League'],
+                },
+              ],
+            },
+            {
+              winner: undefined,
+              poll: undefined,
+              options: [
+                {
+                  winner: undefined,
+                  poll: undefined,
+                  options: ['Lara Croft', 'Poirot'],
+                },
+                {
+                  winner: undefined,
+                  poll: undefined,
+                  options: ['Pulp Heroes', 'Draculas'],
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+  ],
+};
+
+export default data;
---
+++ src/brackets/data/2024.ts
@@ -0,0 +1,144 @@
+import { Data } from 'brackets/types';
+
+// polls https://www.blankcheckpod.com/march-madness
+const data: Data<0> = {
+  winner: undefined,
+  options: [
+    {
+      winner: undefined,
+      options: [
+        {
+          winner: undefined,
+          options: [
+            {
+              winner: undefined,
+              options: [
+                {
+                  winner: 'Steven Soderbergh',
+                  options: ['Steven Soderbergh', 'Barry Levinson'],
+                },
+                {
+                  winner: 'Isao Takahata',
+                  options: ['James Gray', 'Isao Takahata'],
+                },
+              ],
+            },
+            {
+              winner: undefined,
+              options: [
+                {
+                  winner: 'Edgar Wright',
+                  options: ['Edgar Wright', 'Leos Carax'],
+                },
+                {
+                  winner: undefined,
+                  options: ['James Wan', 'Nicole Holofcener'],
+                },
+              ],
+            },
+          ],
+        },
+        {
+          winner: undefined,
+          options: [
+            {
+              winner: undefined,
+              options: [
+                {
+                  winner: 'David Lynch',
+                  options: ['David Lynch', 'Bill Duke'],
+                },
+                {
+                  winner: 'Harold Ramis',
+                  options: ['Richard Kelly', 'Harold Ramis'],
+                },
+              ],
+            },
+            {
+              winner: undefined,
+              options: [
+                {
+                  winner: undefined,
+                  options: ['Denis Villeneuve', 'Curtis Hanson'],
+                },
+                {
+                  winner: undefined,
+                  options: ['Wes Craven', 'Jonathan Glazer'],
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+    {
+      winner: undefined,
+      options: [
+        {
+          winner: undefined,
+          options: [
+            {
+              winner: undefined,
+              options: [
+                {
+                  winner: 'Ridley Scott',
+                  options: ['Ridley Scott', 'Alan Pakula'],
+                },
+                {
+                  winner: 'Andrew Stanton',
+                  options: ['Andrew Stanton', 'Jim Jarmusch'],
+                },
+              ],
+            },
+            {
+              winner: undefined,
+              options: [
+                {
+                  winner: undefined,
+                  options: ['Todd Haynes', 'James Mangold'],
+                },
+                {
+                  winner: undefined,
+                  options: ['Judd Apatow', 'Alex Garland'],
+                },
+              ],
+            },
+          ],
+        },
+        {
+          winner: undefined,
+          options: [
+            {
+              winner: undefined,
+              options: [
+                {
+                  winner: 'Spike Lee',
+                  options: ['Spike Lee', 'Joe Wright'],
+                },
+                {
+                  winner: 'John Hughes',
+                  options: ['John Hughes', 'Céline Sciamma'],
+                },
+              ],
+            },
+            {
+              winner: undefined,
+              options: [
+                {
+                  winner: undefined,
+                  options: ['Richard Linklater', 'Joan Micklin Silver'],
+                },
+                {
+                  winner: undefined,
+                  options: ['OZ/Henson', 'Luca Guadagnino'],
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+  ],
+};
+
+export default data;
