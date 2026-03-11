525e5115834da046b46945f882927a9cc9196166
Jeffrey Burt <jeffrey.m.burt@gmail.com>
2026-03-11 08:00:15 -0700

    chore: winners (1 new)

--- src/brackets/data/2026.ts
+++ src/brackets/data/2026.ts
@@ -83,6 +83,7 @@ const data: Data<0> = {
                   options: ['Wes Anderson', 'James Gray'],
                 },
                 {
+                  winner: 'Steve McQueen',
                   poll: 'https://poll.fm/16667692',
                   options: ['Steve McQueen', 'Jacques Tati'],
                 },
