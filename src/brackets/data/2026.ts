ef53345fdb5983e9f70560760f6d1c00ad61e02c
Jeffrey Burt <jeffrey.m.burt@gmail.com>
2026-03-18 09:21:56 -0700

    chore: winners (1 new)

--- src/brackets/data/2026.ts
+++ src/brackets/data/2026.ts
@@ -8,6 +8,7 @@ const data: Data<0> = {
         {
           options: [
             {
+              winner: 'Martin Scorsese',
               poll: 'https://poll.fm/16734155',
               options: [
                 {
