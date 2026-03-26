7a438bcc80853e02e3f68b203ec9d542805d902c
Jeffrey Burt <jeffrey.m.burt@gmail.com>
2026-03-26 07:05:35 -0700

    chore: winners (1 new)

--- src/brackets/data/2026.ts
+++ src/brackets/data/2026.ts
@@ -43,6 +43,7 @@ const data: Data<0> = {
           ],
         },
         {
+          poll: 'https://poll.fm/16798216',
           options: [
             {
               winner: 'Penelope Spheeris',
